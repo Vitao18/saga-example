5e8be8f680f63b7b088523364c2f45827d193a48
Vitao18 <victoronline9@gmail.com>
2018-02-12 14:33:03 -0200

    refactored app component

--- AddAndSubtract/src/components/App.js
+++ AddAndSubtract/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class App extends React.Component {
-  render() {
-    return (<h1>Hello World!</h1>);
-  }
-}
+const App = ({ counter }) => <div>{counter}</div>;
+
+App.propTypes = {
+  counter: PropTypes.number.isRequired,
+};
 
 export default App;
