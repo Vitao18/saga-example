9aaae8b8622d83a56a9d715aa7f6b9d428ed4bc0
Vitao18 <victoronline9@gmail.com>
2018-02-15 15:35:38 -0200

    added babel and react

--- ToDoList/webpack.config.js
+++ ToDoList/webpack.config.js
@@ -12,8 +12,18 @@ module.exports = {
       {
         test: /\.css%/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
       }
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
