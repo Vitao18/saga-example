a3d11122ad020a4b78cff8f35f6f6936cd327a8e
Vitao18 <victoronline9@gmail.com>
2018-02-16 11:15:29 -0200

    added basic react-hot-loader configuration

--- ToDoList/src/index.js
+++ ToDoList/src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader';
+import App from './Components/App';
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+  document.getElementById('root'),
+  )
+}
+  
+render(App)
+  
+  // Webpack Hot Module Replacement API
+if (module.hot) module.hot.accept('./Components/App', () => { render(App) });
--- ToDoList/webpack.config.js
+++ ToDoList/webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -26,6 +32,8 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
     }),
